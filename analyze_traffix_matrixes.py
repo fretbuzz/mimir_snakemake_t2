96e6a67586f86c1f443ee53d2206029897157c54
Joseph Severini <jseverin@GS17007.SP.CS.CMU.EDU>
2018-04-03 11:08:00 -0400

    Iterate through relevant dataframe values

--- analyze_traffix_matrixes.py
+++ analyze_traffix_matrixes.py
@@ -2,10 +2,40 @@ import pickle
 import pandas as pd
 import numpy as np
 
+# TODO: see if the send-recieved pairs match up as we'd expect them to
 # assume that I have the pickled data frames
 # unpickle the frames
 # find the differentials
-# compute the statistics that I want:
+# make the time column an index (maybe don't need to do this)
+# first statistic that I want: control chart. 
+#   How to get this: 
+#       (1) need to select relevant entries for each pod pair
+#           need to condition on values OTHER than time
+#           what values to select? need to select rows that match particular vlaues
+#               then select some of the resulting columns
+#       (2) for now just use .describe() to see a bunch of stats, can customize in a little while
+# second statistic that I want:
+#   PCA
+
+services = [
+        'carts',
+        'carts-db',
+        'catalogue',
+        'catalogue-db',
+        'front-end',
+        'orders',
+        'orders-db',
+        'payment',
+        'queue-master',
+        'rabbitmq',
+        'session-db',
+        'shipping',
+        'user',
+        'user-db',
+        'load-test',
+        '127.0.0.1', #it's always there, so I guess we should treat it like a normal thing
+        '172.17.0.1' # also this one too
+]
 
 def main():
     print "hello world"
@@ -14,6 +44,12 @@ def main():
 
     print "Here is the sent traffic matrixes"
     print df_sent
+    for index_service in services:
+        for column_service in services:
+            # NOTE: this is where I'd condition on time values, if I wanted to do
+            # like a moving average or something
+            relevant_traffic_values = df_sent.loc[index_service, column_service]
+            print relevant_traffic_values
 
     print "Here is the recieved traffic matrixes"
     print df_rec
