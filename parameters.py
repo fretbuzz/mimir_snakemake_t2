09e3a7d6acef053e1647743f6c5ae8768324681c
fretbuzz <jseverin@andrew.cmu.edu>
2018-04-10 16:31:36 -0400

    going to see if this new metric is nonsense or not

--- parameters.py
+++ parameters.py
@@ -13,25 +13,25 @@ rate_spawn_background_locusts = "1" # /sec
 # this is how long the experiment goes before data is startng to be
 # exfiltrated (could be randomized later on??)
 # note: make this number negative of you want exfiltration to NEVER happen
-desired_exfil_time = -10 #60
+desired_exfil_time = 90 #60
 
 # this is the total length of the experiment
 # so the experiment keeps running for 
 # desired_stop_time - desired_exfil_time
 # after data exfiltration has taken place
-desired_stop_time = 60 #100
+desired_stop_time = 180
 
 # this is the name of the pickle file where the recieved
 # traffic matrixes will be kept
 # note: this should always end in .pickle
 # note: './experimental_data/' will be pre-pended to this
-sent_matrix_location = "cumul_sent_matrix_no_exfil.pickle"
+sent_matrix_location = "cumul_sent_matrix_total_bytes_exfil_at_90.pickle"
 
 # this is the name of the pickle file where the sent
 # traffic matrixes wil be kept
 # note: this should always end in .pickle
 # note: './experimental_data/' will be pre-pended to this
-rec_matrix_location = "cumul_recieved_matrix_no_exfil.pickle"
+rec_matrix_location = "cumul_recieved_matrix_total_bytes_exfil_at_90.pickle"
 
 ## this is a list of the SENT svc_pair graphs that are displyed
 ## Note: this list should be 1,2, or 4 items long
