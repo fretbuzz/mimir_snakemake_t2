ca1f2a8040f7a7e7909927bf0b3f601d9f662cb8
fretbuzz <jseverin@andrew.cmu.edu>
2018-06-05 14:47:15 -0400

    uses repo now

--- start_with_istio.sh
+++ start_with_istio.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-kubectl create -f ./manifests/sock-shop-ns.yaml
-for filename in ./manifests/*; do
+kubectl create -f ./microservices-demo/munnin/microservices-demo/deploy/kubernetes/manifests/sock-shop-ns.yaml
+for filename in ./microservices-demo/munnin/microservices-demo/deploy/kubernetes/manifests/*; do
     echo $filename
     kubectl apply -f <($1/bin/istioctl kube-inject -f $filename)
 done
