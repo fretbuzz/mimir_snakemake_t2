6426cf74d500bfbd77472d645dae95fd75b6f715
jsev <jsev@node.munnin4.dna-pg0.wisc.cloudlab.us>
2018-06-08 11:57:28 -0500

    for installing petstore

---
+++ install_scripts/install_petstore.sh
@@ -0,0 +1,6 @@
+sudo bash ../install_docker_compose.sh
+git clone https://github.com/micronaut-projects/micronaut-examples.git 
+cd ./micronaut-examples/petstore/
+./gradlew build -x test # and wait a long time
+sudo docker-compose build
+sudo docker-compose up
