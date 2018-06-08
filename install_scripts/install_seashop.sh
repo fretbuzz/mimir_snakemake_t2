95c572eb693f7de2159fde0d3379000e52620db5
jsev <jsev@node.munnin4.dna-pg0.wisc.cloudlab.us>
2018-06-08 15:51:06 -0500

    more tweaking of install scripts

--- install_scripts/install_seashop.sh
+++ install_scripts/install_seashop.sh
@@ -13,6 +13,7 @@ mkdir certs
   echo '\n';
 } | openssl req -newkey rsa:4096 -nodes -sha256 -keyout certs/domain.key -x509 -days 365 -out certs/domain.crt
 
+docker swarm init
 sudo docker secret create revprox_cert certs/domain.crt
 sudo docker secret create revprox_key certs/domain.key
 sudo docker secret create postgres_password certs/domain.key
