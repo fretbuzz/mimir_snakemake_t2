15e5c046087ce8e0d00cfe09a3e83ae1daf735a8
fretbuzz <jseverin@andrew.cmu.edu>
2018-06-14 15:18:47 -0400

    load generators

--- load_generators/petstore_test.py
+++ load_generators/petstore_test.py
@@ -11,7 +11,18 @@ print r.text
 
 # this actualy seems not to complicated... okay which components are here
 # TODO: (1) figure out how to autoscale docker compose
-#       (2) thingso to go to to stress site:
+            # forget about autoscaling, let's just specify it in docker compose up --scale flag
+            # too complicated w/ just compose. Easier w/ swarm
+            # b/c automatic load balancing but need to modify
+            # yaml file to save stuff to a local registry
+            # note: I modified the compose file
+            # note I think I need to make the image refernce
+            # the local registry and not just use a name b/c
+            # it looks for it in the cloud if local is not
+            # specified (See https://docs.docker.com/engine/swarm/stack-deploy/#create-the-example-application)
+            # okay
+#      (2) things to go to to stress site:
+            # so I gotta test the code that I wrote below
 # http://localhost:3000/, http://localhost:3000/pets, http://localhost:3000/vendors, http://localhost:3000/about
 # http://localhost:3000/pets/crabbe (so take each of the results from pets and then go to their 'more info' page)
 # also need to send some emails; looks like the way to go about doing this is to post to http://localhost:8080/mail/send
@@ -19,7 +30,6 @@ print r.text
 # also need to send some comments
 
 # note there is no setup required for this site, just background traffic
-
 
 ''' # related to send emails
 Request URL: http://localhost:8080/mail/send
@@ -95,16 +105,72 @@ poster
 :
 "bob"
 
-
-
 '''
-
 ## okay, so what services are we being sure to exercise
-## vendors (yes, b/c looking @ vendors)
-## offers (yes, b/c on homepage)
-## comments (TODO: NO??)
-## functions (yes, automatically)
-## email (yes, register emails)
-## pets (yes, looking @ pets)
-## front-end (yes, automatically)
-## storefront (yes, automatically)
+## vendors (yes, b/c looking @ vendors) (yes_
+## offers (yes, b/c on homepage) (yes)
+## comments (yes, registering comments (see above) (yes)
+## functions (yes, automatically) (yes)
+## email (yes, register emails) (yes)
+## pets (yes, looking @ pets) (yes)
+## front-end (yes, automatically) (yes)
+## storefront (yes, automatically) (yes)
+
+
+# http://localhost:3000/, http://localhost:3000/pets, http://localhost:3000/vendors, http://localhost:3000/about
+
+
+s = requests.Session()
+
+r = s.get('http://localhost:3000/')
+print r.text
+
+print "########"
+
+
+r = s.get('http://localhost:3000/about')
+print r.text
+
+print "########"
+
+r = s.get('http://localhost:3000/vendors')
+print r.text
+
+print "########"
+
+r = s.get('http://localhost:3000/pets')
+print r.text
+
+pets = r.json()
+print pets
+
+pet = random.choice(pets)
+
+print "########"
+
+r = s.get('http://localhost:3000/pets' + pet)
+print r.text
+
+print "########"
+
+# now email
+
+# where crabbe should be replaced
+r = s.post('http://localhost:8080/mail/send', data={'email': "bob@bob.com", 'slug': pet},
+           headers={'referer': 'http://localhost:3000/pets/' + pet +'?email='})
+
+print r.text
+
+print "########"
+
+# now comments
+
+r = s.post('http://localhost:8080/comment/' + pet, data={'poster': "bob", 'content': "bobbert"},
+           headers={'referer': 'http://localhost:3000/pets/' + pet})
+
+print r.text
+
+# so the next thing to do would be to put this into a locust file
+
+# neo4j, mongodb, redis, pets, vendors, offers, comments,
+# mail, storefront, frontend
