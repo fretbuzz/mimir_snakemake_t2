53322dd2ca7297e6e83faa05f2b37687a1c884b2
jsev <jsev@node.mimir.dna-pg0.wisc.cloudlab.us>
2018-08-22 09:08:13 -0500

    connections try to make ipv6 connections with cilium, which is bad

--- install_scripts/apt_det_dependencies.sh
+++ install_scripts/apt_det_dependencies.sh
@@ -1,7 +1,7 @@
-apt-get update
+apt-get -o Acquire::ForceIPv4=true update
 export DEBIAN_FRONTEND=noninteractive
-apt-get --force-yes -y install git python curl python-dev gcc
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+apt-get --force-yes -y -o Acquire::ForceIPv4=true install git python curl python-dev gcc
+curl --ipv4 https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python get-pip.py
 git clone https://github.com/fretbuzz/DET /DET
 pip install -r /DET/requirements.txt --user
