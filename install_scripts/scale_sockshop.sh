d1342b260f68d99095e4447c8c63b1bfd2290f9c
jsev <jsev@node.mimir2.dna-pg0.clemson.cloudlab.us>
2018-09-07 20:55:46 -0400

    better

--- install_scripts/scale_sockshop.sh
+++ install_scripts/scale_sockshop.sh
@@ -1,12 +1,16 @@
 
-declare -i current_carts_instances=1
+declare -i current_carts_instances=4
 declare -i goal_carts_instnaces=10
 
 kubectl scale deploy catalogue orders payment queue-master shipping user front-end --replicas=10 --namespace="sock-shop"
+sleep 300
+kubectl scale deploy carts --replicas=4 --namespace="sock-shop"
+sleep 360
+
 while [ $current_carts_instances -lt $goal_carts_instances ] 
 do
-current_carts_instances=$current_carts_instances+3
+current_carts_instances=$current_carts_instances+1
 kubectl scale deploy carts "--replicas=$current_carts_instances" --namespace="sock-shop"
 
-sleep 360
+sleep 240
 done
