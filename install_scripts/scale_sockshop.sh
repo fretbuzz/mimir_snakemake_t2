f574dec56336ac19fb07a9c638ac1c0791eaeee8
root <root@node.mimir2.dna-pg0.clemson.cloudlab.us>
2018-09-14 19:44:00 -0400

    fixed tyop

--- install_scripts/scale_sockshop.sh
+++ install_scripts/scale_sockshop.sh
@@ -1,6 +1,6 @@
 
 declare -i current_carts_instances=4
-declare -i goal_carts_instnaces=10
+declare -i goal_carts_instances=10
 
 kubectl scale deploy catalogue orders payment queue-master shipping user front-end --replicas=10 --namespace="sock-shop"
 sleep 420
