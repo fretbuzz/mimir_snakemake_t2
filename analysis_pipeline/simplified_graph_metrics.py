0101894dac8ee2fd91d70f5488d000907ec3848e
fretbuzz <jseverin@andrew.cmu.edu>
2019-01-14 14:15:28 -0500

    made (untested) modifications to save edgefile for injected graph

--- analysis_pipeline/simplified_graph_metrics.py
+++ analysis_pipeline/simplified_graph_metrics.py
@@ -18,6 +18,7 @@ import logging
 import time
 from matplotlib import pyplot as plt
 from networkx.drawing.nx_agraph import graphviz_layout
+import os,errno
 
 # okay, so things to be aware of:
 # (a) we are assuming that if we cannot label the node and it is not loopback or in the '10.X.X.X' subnet, then it is outside
@@ -98,6 +99,21 @@ def calc_subset_graph_metrics(filenames, time_interval, basegraph_name, calc_val
             cur_1si_G = prepare_graph(G, svcs, 'app_only', is_swarm, counter, file_path, ms_s, container_to_ip,
                                   infra_service)
 
+            # let's save the processed version of the graph in a nested folder for easier comparison during the
+            # debugging process... and some point I could even decouple creating/processing the edgefiles and
+            # calculating the corresponding graph metrics
+            edgefile_folder_path = "/".join(file_path.split('/')[:-1])
+            name_of_file = file_path.split('/')[-1]
+            edgefile_pruned_folder_path = edgefile_folder_path + '/pruned_edgefiles/'
+            ## if the pruned folder directory doesn't currently exist, then we'd want to create it...
+            ## using the technique from https://stackoverflow.com/questions/273192/how-can-i-safely-create-a-nested-directory-in-python
+            try:
+                os.makedirs(edgefile_pruned_folder_path)
+            except OSError as e:
+                if e.errno != errno.EEXIST:
+                    raise
+            nx.write_edgelist(cur_1si_G, edgefile_pruned_folder_path+name_of_file, data=['frames', 'weight'])
+
             cur_class_G = prepare_graph(G, svcs, 'class', is_swarm, counter, file_path, ms_s, container_to_ip,
                                   infra_service)
 
@@ -135,6 +151,17 @@ def calc_subset_graph_metrics(filenames, time_interval, basegraph_name, calc_val
                                                                         pre_specified_data_attribs, name_of_dns_pod_node,
                                                                            avg_dns_weight, avg_dns_pkts)
 
+            # let's save a copy of the edgefile for the graph w/ the injected attack b/c that'll help with debugging
+            # the system...
+            edgefile_injected_folder_path = edgefile_folder_path + '/injected_edgefiles/'
+            ## if the injected folder directory doesn't currently exist, then we'd want to create it...
+            ## using the technique from https://stackoverflow.com/questions/273192/how-can-i-safely-create-a-nested-directory-in-python
+            try:
+                os.makedirs(edgefile_injected_folder_path)
+            except OSError as e:
+                if e.errno != errno.EEXIST:
+                    raise
+            nx.write_edgelist(cur_1si_G, edgefile_injected_folder_path+name_of_file, data=['frames', 'weight'])
 
             ##continue ### <<<----- TODO: remove!
             #exit() #### <----- TODO: remove!!
