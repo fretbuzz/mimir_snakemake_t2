72093f7b25fc26dc1f9614d05a0b0ab2153e65de
jsev <jsev@node.mimir-1.dna-pg0.wisc.cloudlab.us>
2019-02-18 07:25:17 -0600

    added missing selenium dependencies

--- experiment_coordinator/install_scripts/install_selenium_dependencies.sh
+++ experiment_coordinator/install_scripts/install_selenium_dependencies.sh
@@ -3,4 +3,8 @@ wget https://ftp.mozilla.org/pub/firefox/releases/63.0/linux-x86_64/en-US/firefo
 tar -xvf firefox-63.0.tar.bz2
 cd ./firefox/
 sudo cp -R . /usr/local/bin/
+wget https://github.com/mozilla/geckodriver/releases/download/v0.24.0/geckodriver-v0.24.0-linux64.tar.gz
+tar -xvf geckodriver-v0.24.0-linux64.tar.gz 
+cp ./geckodriver /usr/local/bin/
+pip pip install selenium
 firefox --version #to check that it is worknig
