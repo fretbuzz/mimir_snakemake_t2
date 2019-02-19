af4bb4892a4967ded6bc4a598cd8c3ca8bec2e6e
root <root@node.mimir-1.dna-pg0.wisc.cloudlab.us>
2019-02-19 06:43:55 -0600

    fixed bug in selenium install instructions

--- experiment_coordinator/install_scripts/install_selenium_dependencies.sh
+++ experiment_coordinator/install_scripts/install_selenium_dependencies.sh
@@ -6,5 +6,5 @@ sudo cp -R . /usr/local/bin/
 wget https://github.com/mozilla/geckodriver/releases/download/v0.20.1/geckodriver-v0.20.1-linux64.tar.gz
 tar -xvf geckodriver-v0.20.1-linux64.tar.gz 
 cp ./geckodriver /usr/local/bin/
-pip pip install selenium
+pip install selenium
 firefox --version #to check that it is worknig
