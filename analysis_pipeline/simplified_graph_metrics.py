b2b0632f1afa18b5b0f2df802d7cb257c340c469
fretbuzz <jseverin@andrew.cmu.edu>
2019-03-01 11:44:19 -0500

    made determination of exfiltration change amt it's own function

--- analysis_pipeline/simplified_graph_metrics.py
+++ analysis_pipeline/simplified_graph_metrics.py
@@ -610,7 +610,7 @@ def inject_synthetic_attacks(graph, synthetic_exfil_paths, initiator_info_for_pa
         return graph, attack_number_to_mapping, {'weight': fraction_of_weight_min,
                                                  'frames': fraction_of_pkt_min}, concrete_node_path, carryover
 
-    # not VIPs in class granularity graph for some reason
+    # no VIPs in class granularity graph for some reason
     if node_granularity == 'class':
         synthetic_exfil_paths = copy.deepcopy(synthetic_exfil_paths)
         remaining_node_path = []
@@ -619,111 +619,11 @@ def inject_synthetic_attacks(graph, synthetic_exfil_paths, initiator_info_for_pa
                 remaining_node_path.append(node)
         synthetic_exfil_paths[attack_occuring] = remaining_node_path
 
-    # step (2): is this the first contiguous time iteration that this kind of attack occurs?
-    # if yes -> must determine the node mapping now
-    # if no -> use node mapping from before (requires no work b/c needed info already in dict)
-    if attack_occuring not in attack_number_to_mapping.keys() or last_attack != attack_occuring:
-        current_mapping = {} # abstract_node -> concrete_node
-        for node in synthetic_exfil_paths[attack_occuring]:
-            # if node not in current_mapping.keys() # doesn't actually matter...
-            if node == 'kube_dns_pod':
-                print "kube_dns_pod found in mapping function!!", name_of_dns_pod_node
-                current_mapping['kube_dns_pod'] = name_of_dns_pod_node
-            else:
-                concrete_node = abstract_to_concrete_mapping(node, graph, node_granularity)
-                if 'dns' in node:
-                    print "new_mapping!", node, concrete_node
-                current_mapping[node] = concrete_node
-        attack_number_to_mapping[attack_occuring] = current_mapping
 
-    # step (3) : determine the appropriate weight change for the edges leading to exfiltration
-    # note that if we are working at class node granularity, then we should just use the weights
-    # determined for the corresponding pod granularity graph
-    if node_granularity != 'class':
-        all_weights_in_exfil_path = []
-        all_pkts_in_exfil_path = []
-        #abstract_node_pair = None
-        #concrete_node_pair = None
-        first_concrete_node_pair = None
-        dns_exfil_path = False
-        for node_one_loc in range(0, len(synthetic_exfil_paths[attack_occuring]) -1 ):
-            abstract_node_pair = (synthetic_exfil_paths[attack_occuring][node_one_loc],
-                                  synthetic_exfil_paths[attack_occuring][node_one_loc+1])
-
-            if 'dns_vip' in abstract_node_pair[1]:
-                logging.info("dns_vip found in abstract node pair")
-                dns_exfil_path = True
-                # then let's clear all the early values... since they're all giant probably...
-                all_weights_in_exfil_path = []
-                all_pkts_in_exfil_path = []
-
-            concrete_node_src = attack_number_to_mapping[attack_occuring][abstract_node_pair[0]]
-            concrete_node_dst = attack_number_to_mapping[attack_occuring][abstract_node_pair[1]]
-            concrete_node_pair = (concrete_node_src,concrete_node_dst)
-
-            if not first_concrete_node_pair:
-                first_concrete_node_pair = concrete_node_pair
-
-            # wanna determine the relevant weight and then add it. (so in actualuality, this is #3 from below)
-            print "concrete_nodes", concrete_node_src,concrete_node_dst
-            concrete_edge = graph.get_edge_data(concrete_node_src, concrete_node_dst)
-            print concrete_edge
-            cur_exfil_weight = None
-            if concrete_edge: # will return None if edge doesn't exist...
-                cur_exfil_weight = concrete_edge['weight']
-                all_weights_in_exfil_path.append( cur_exfil_weight )
-                all_pkts_in_exfil_path.append( concrete_edge['frames'] )
-            else:
-                logging.info("need to find an equivalent edge to: " + str(concrete_node_src) + " " + str(concrete_node_dst))
-                equivalent_edge = find_equiv_edge(concrete_node_pair, graph, node_granularity)
-                if equivalent_edge:
-                    logging.info("this equivalent_edge is:" + str(equivalent_edge[0]) + ', ' + str(equivalent_edge[1]))
-                    equiv_concrete_node_src = equivalent_edge[0]
-                    equiv_concrete_node_dst = equivalent_edge[1]
-                    cur_exfil_data  = graph.get_edge_data(equiv_concrete_node_src, equiv_concrete_node_dst)
-                    cur_exfil_weight = cur_exfil_data['weight']
-                    cur_exfil_frames = cur_exfil_data['frames']
-                    all_weights_in_exfil_path.append( cur_exfil_weight )
-                    all_pkts_in_exfil_path.append( cur_exfil_frames )
-                else:
-                    if 'dns' in concrete_node_dst or 'dns' in concrete_node_src:
-                        logging.info("no equivalent edge found and dns is in the name... must be dns")
-                        # equivalent_edge is only None when it's a dns_exfil path and no dns nodes are present in the current graph
-                        # in which case, we'll have to rely on previous dns behavior
-                        # (this previous behavior will be passed as a parameter to the function...)
-                        cur_exfil_weight = avg_dns_weight
-                        all_weights_in_exfil_path.append(avg_dns_weight)
-                        all_pkts_in_exfil_path.append(avg_dns_pkts)
-
-            logging.info("found this weight: " + str(cur_exfil_weight))
-        logging.info("all_weights_in_exfil_path" + str(all_weights_in_exfil_path) + ", time_gran: " + str(time_granularity))
-        # so let's choose the weight/packets... let's maybe go w/ some fraction of the median...
-        pkt_np_array = np.array(all_pkts_in_exfil_path)
-        weight_np_array = np.array(all_weights_in_exfil_path)
-        pkt_min = np.min(pkt_np_array)
-        weight_min =  np.min(weight_np_array)
-
-        logging.info("weight_min: " + str(weight_min))
-
-        if not dns_exfil_path:
-            fraction_of_pkt_min = max(int(math.ceil(pkt_min * fraction_of_edge_pkts)),1)
-            fraction_of_weight_min = int(weight_min * fraction_of_edge_weights)+ old_carryover
-        else:
-            # NOTE THE 10* HERE
-            fraction_of_pkt_min = max(int(math.ceil(pkt_min * 10.0 * fraction_of_edge_pkts)),1)
-            fraction_of_weight_min = int(weight_min * 10.0 * fraction_of_edge_weights)+ old_carryover
-
-        logging.info("fraction_of_weight_min: " + str(fraction_of_weight_min) + ";; fraction_of_edge_weights: " + str(
-            fraction_of_edge_weights))
-        attack_occuring_str = " ".join([str(z) for z in synthetic_exfil_paths[attack_occuring]])
-        print("attack_occuring", attack_occuring_str)
-        logging.info("attack_occuring: " + attack_occuring_str)
-
-    else:
-        ###  we should store the corresponding attribs from the app_only granularity and then just
-        # use that (b/c class gran. gives super huge).
-        fraction_of_pkt_min = pre_specified_data_attribs['frames']
-        fraction_of_weight_min = pre_specified_data_attribs['weight']
+    fraction_of_pkt_min, fraction_of_weight_min = \
+        determine_exfiltration_amt(pre_specified_data_attribs, attack_occuring, attack_number_to_mapping,
+        last_attack, synthetic_exfil_paths, name_of_dns_pod_node, node_granularity, avg_dns_weight,
+        avg_dns_pkts, time_granularity, fraction_of_edge_pkts, fraction_of_edge_weights, old_carryover)
 
     ## Step (4): use the previously calculated exfiltration rate to actually make the appropriate modifications
     ## to the graph.
@@ -1015,6 +915,119 @@ def avg_behavior_into_dns_node(pre_injection_weight_into_dns_dict, pre_inject_pa
     #    avg_dns_pkts = 0.0
 
     return avg_dns_weight, avg_dns_pkts
+
+def determine_exfiltration_amt(pre_specified_data_attribs, attack_occuring, attack_number_to_mapping,
+                               last_attack, synthetic_exfil_paths, name_of_dns_pod_node, node_granularity,
+                               avg_dns_weight, avg_dns_pkts, time_granularity, fraction_of_edge_pkts,
+                               fraction_of_edge_weights, old_carryover):
+
+    # step (2): is this the first contiguous time iteration that this kind of attack occurs?
+    # if yes -> must determine the node mapping now
+    # if no -> use node mapping from before (requires no work b/c needed info already in dict)
+    if attack_occuring not in attack_number_to_mapping.keys() or last_attack != attack_occuring:
+        current_mapping = {} # abstract_node -> concrete_node
+        for node in synthetic_exfil_paths[attack_occuring]:
+            # if node not in current_mapping.keys() # doesn't actually matter...
+            if node == 'kube_dns_pod':
+                print "kube_dns_pod found in mapping function!!", name_of_dns_pod_node
+                current_mapping['kube_dns_pod'] = name_of_dns_pod_node
+            else:
+                concrete_node = abstract_to_concrete_mapping(node, graph, node_granularity)
+                if 'dns' in node:
+                    print "new_mapping!", node, concrete_node
+                current_mapping[node] = concrete_node
+        attack_number_to_mapping[attack_occuring] = current_mapping
+
+    # step (3) : determine the appropriate weight change for the edges leading to exfiltration
+    # note that if we are working at class node granularity, then we should just use the weights
+    # determined for the corresponding pod granularity graph
+    if node_granularity != 'class':
+        all_weights_in_exfil_path = []
+        all_pkts_in_exfil_path = []
+        #abstract_node_pair = None
+        #concrete_node_pair = None
+        first_concrete_node_pair = None
+        dns_exfil_path = False
+        for node_one_loc in range(0, len(synthetic_exfil_paths[attack_occuring]) -1 ):
+            abstract_node_pair = (synthetic_exfil_paths[attack_occuring][node_one_loc],
+                                  synthetic_exfil_paths[attack_occuring][node_one_loc+1])
+
+            if 'dns_vip' in abstract_node_pair[1]:
+                logging.info("dns_vip found in abstract node pair")
+                dns_exfil_path = True
+                # then let's clear all the early values... since they're all giant probably...
+                all_weights_in_exfil_path = []
+                all_pkts_in_exfil_path = []
+
+            concrete_node_src = attack_number_to_mapping[attack_occuring][abstract_node_pair[0]]
+            concrete_node_dst = attack_number_to_mapping[attack_occuring][abstract_node_pair[1]]
+            concrete_node_pair = (concrete_node_src,concrete_node_dst)
+
+            if not first_concrete_node_pair:
+                first_concrete_node_pair = concrete_node_pair
+
+            # wanna determine the relevant weight and then add it. (so in actualuality, this is #3 from below)
+            print "concrete_nodes", concrete_node_src,concrete_node_dst
+            concrete_edge = graph.get_edge_data(concrete_node_src, concrete_node_dst)
+            print concrete_edge
+            cur_exfil_weight = None
+            if concrete_edge: # will return None if edge doesn't exist...
+                cur_exfil_weight = concrete_edge['weight']
+                all_weights_in_exfil_path.append( cur_exfil_weight )
+                all_pkts_in_exfil_path.append( concrete_edge['frames'] )
+            else:
+                logging.info("need to find an equivalent edge to: " + str(concrete_node_src) + " " + str(concrete_node_dst))
+                equivalent_edge = find_equiv_edge(concrete_node_pair, graph, node_granularity)
+                if equivalent_edge:
+                    logging.info("this equivalent_edge is:" + str(equivalent_edge[0]) + ', ' + str(equivalent_edge[1]))
+                    equiv_concrete_node_src = equivalent_edge[0]
+                    equiv_concrete_node_dst = equivalent_edge[1]
+                    cur_exfil_data  = graph.get_edge_data(equiv_concrete_node_src, equiv_concrete_node_dst)
+                    cur_exfil_weight = cur_exfil_data['weight']
+                    cur_exfil_frames = cur_exfil_data['frames']
+                    all_weights_in_exfil_path.append( cur_exfil_weight )
+                    all_pkts_in_exfil_path.append( cur_exfil_frames )
+                else:
+                    if 'dns' in concrete_node_dst or 'dns' in concrete_node_src:
+                        logging.info("no equivalent edge found and dns is in the name... must be dns")
+                        # equivalent_edge is only None when it's a dns_exfil path and no dns nodes are present in the current graph
+                        # in which case, we'll have to rely on previous dns behavior
+                        # (this previous behavior will be passed as a parameter to the function...)
+                        cur_exfil_weight = avg_dns_weight
+                        all_weights_in_exfil_path.append(avg_dns_weight)
+                        all_pkts_in_exfil_path.append(avg_dns_pkts)
+
+            logging.info("found this weight: " + str(cur_exfil_weight))
+        logging.info("all_weights_in_exfil_path" + str(all_weights_in_exfil_path) + ", time_gran: " + str(time_granularity))
+        # so let's choose the weight/packets... let's maybe go w/ some fraction of the median...
+        pkt_np_array = np.array(all_pkts_in_exfil_path)
+        weight_np_array = np.array(all_weights_in_exfil_path)
+        pkt_min = np.min(pkt_np_array)
+        weight_min =  np.min(weight_np_array)
+
+        logging.info("weight_min: " + str(weight_min))
+
+        if not dns_exfil_path:
+            fraction_of_pkt_min = max(int(math.ceil(pkt_min * fraction_of_edge_pkts)),1)
+            fraction_of_weight_min = int(weight_min * fraction_of_edge_weights)+ old_carryover
+        else:
+            # NOTE THE 10* HERE
+            fraction_of_pkt_min = max(int(math.ceil(pkt_min * 10.0 * fraction_of_edge_pkts)),1)
+            fraction_of_weight_min = int(weight_min * 10.0 * fraction_of_edge_weights)+ old_carryover
+
+        logging.info("fraction_of_weight_min: " + str(fraction_of_weight_min) + ";; fraction_of_edge_weights: " + str(
+            fraction_of_edge_weights))
+        attack_occuring_str = " ".join([str(z) for z in synthetic_exfil_paths[attack_occuring]])
+        print("attack_occuring", attack_occuring_str)
+        logging.info("attack_occuring: " + attack_occuring_str)
+
+    else:
+        ###  we should store the corresponding attribs from the app_only granularity and then just
+        # use that (b/c class gran. gives super huge).
+        fraction_of_pkt_min = pre_specified_data_attribs['frames']
+        fraction_of_weight_min = pre_specified_data_attribs['weight']
+
+    return fraction_of_pkt_min, fraction_of_weight_min
 
 def pairwise_metrics(G, svc_to_nodes):
     svc_pair_to_reciprocity = {}
