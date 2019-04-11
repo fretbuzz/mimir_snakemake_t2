d0614577638eee21c224fda8e85151addb61d364
fretbuzz <jseverin@andrew.cmu.edu>
2019-04-11 03:39:12 -0400

    let's test this

--- experiment_coordinator/hipsterStore_setup/autoscale_hipsterStore.sh
+++ experiment_coordinator/hipsterStore_setup/autoscale_hipsterStore.sh
@@ -4,7 +4,7 @@
 
 kubectl autoscale deployment adservice --cpu-percent=80 --min=1 --max=10
 ####
-#kubectl autoscale deployment cartservice --cpu-percent=80 --min=1 --max=10 # doesn't seem to make a difference if autoscaled or not... maybe check again later tho...
+kubectl autoscale deployment cartservice --cpu-percent=80 --min=1 --max=10 # doesn't seem to make a difference if autoscaled or not... maybe check again later tho...
 kubectl autoscale deployment checkoutservice --cpu-percent=80 --min=1 --max=1 # don't autoscale b/c replicas aren't used anyway0
 #####
 kubectl autoscale deployment currencyservice --cpu-percent=80 --min=1 --max=10
