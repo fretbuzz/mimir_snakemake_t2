75085f4569d2736979b28e7b9ac81455d55c0d2b
fretbuzz <jseverin@andrew.cmu.edu>
2019-04-26 14:35:13 -0400

    refactored load generation of sockshop ready for testing

--- experiment_coordinator/sockshop_setup/background_traffic.py
+++ experiment_coordinator/sockshop_setup/background_traffic.py
@@ -7,10 +7,23 @@ import string
 import cPickle as pickle
 import time
 from pop_db import get_random_num, gen_random
+import os
 
 # this way we can simulate users using already-existing accounts
 with open( "users.pickle", "rb" ) as f:
     users = pickle.loads( f.read() )
+
+## TODO: get the distro....
+if os.path.isfile('prob_distro_sock.pickle'):
+    with open('prob_distro_sock.pickle', 'r') as f:
+        prob_distr = pickle.loads(f.read())
+else:
+    ## here's a hypothetical default probability distribution...
+    prob_distr = {'buy': 0.3, 'browse': 0.5, 'register': 0.2}
+
+num_browsing = 21
+min_wait = 2000
+max_wait = 4000
 
 class BackgroundTraffic(TaskSet):
     #'''
@@ -18,30 +31,41 @@ class BackgroundTraffic(TaskSet):
     min_wait = 2000
     max_wait = 4000
 
-    # okay, so the goal here is to simulate an actual user
-    # the user is going to browse for some randomized period of time
-    # and then they are going to buy something with some probability
-    @task(8)
-    def browse(self):
-        min_wait = 2000
-        max_wait = 4000
-        # login before browsing with some probability
-        login_p = randint(0,10)
-        # let's arbitrarily say that 70% of visitors log in
-        if login_p >= 3:
-            # so let's randomly choose some already-registered user
-            user = choice(users)
-            # NOTE: this borrows code from weave's load-test repo
-            base64string = base64.encodestring('%s:%s' % (user, user)).replace('\n', '')
-            self.client.get("/login", headers={"Authorization":"Basic %s" % base64string})
-            time.sleep(randint(min_wait,max_wait) / 1000.0) # going to wait a bit between events            
+    ## note: assuming visitor will only login if they mean to buy osmething...
+    @task(prob_distr['buy'] * 100)
+    def buy(self):
+        self.client.get("/")
+        # first login
+        user = choice(users)
+        # NOTE: this borrows code from weave's load-test repo
+        base64string = base64.encodestring('%s:%s' % (user, user)).replace('\n', '')
+        self.client.get("/login", headers={"Authorization": "Basic %s" % base64string})
+        time.sleep(randint(min_wait, max_wait) / 1000.0)  # going to wait a bit between events
 
-        # browse through the socks for a certain number of times
-        num_browsing = randint(1,21)
-        # NOTE: this borrows  code from weave's load-test repo 
-        item_id = 0 ##
         catalogue = self.client.get("/catalogue").json()
-        for i in range(0, num_browsing):
+        time.sleep(randint(min_wait,max_wait) / 1000.0) # going to wait a bit between events
+        category_item = choice(catalogue)
+        item_id = category_item["id"]
+        self.client.get("/category.html")
+        time.sleep(randint(min_wait,max_wait) / 1000.0) # going to wait a bit between events
+        self.client.get("/detail.html?id={}".format(item_id))
+
+        time.sleep(randint(min_wait, max_wait) / 1000.0)  # going to wait a bit between events
+        # NOTE: this borrows code from weave's load-test repo
+        self.client.delete("/cart")
+        item_num = 1  # randint(1,4) # can't be more than 100 and they have something that is 99.99
+        self.client.post("/cart", json={"id": item_id, "quantity": item_num})
+        time.sleep(randint(min_wait, max_wait) / 1000.0)  # going to wait a bit between events
+        self.client.get("/basket.html")
+        order_post = self.client.post("/orders")
+        print order_post
+        print order_post.text
+
+    @task(prob_distr['browse'] * 100)
+    def browse(self):
+        self.client.get("/")
+        catalogue = self.client.get("/catalogue").json()
+        for i in range(1, num_browsing):
             time.sleep(randint(min_wait,max_wait) / 1000.0) # going to wait a bit between events
             category_item = choice(catalogue)
             item_id = category_item["id"]
@@ -49,22 +73,8 @@ class BackgroundTraffic(TaskSet):
             time.sleep(randint(min_wait,max_wait) / 1000.0) # going to wait a bit between events
             self.client.get("/detail.html?id={}".format(item_id))
 
-        # buy some socks with some probability
-        # let's arbitrarily say that 30% percent of visiters buy some socks
-        if login_p >= 7:
-            time.sleep(randint(min_wait,max_wait) / 1000.0) # going to wait a bit between events
-            # NOTE: this borrows code from weave's load-test repo
-            self.client.delete("/cart")
-            item_num = 1 # randint(1,4) # can't be more than 100 and they have something that is 99.99
-            self.client.post("/cart", json={"id": item_id, "quantity": item_num})
-            time.sleep(randint(min_wait,max_wait) / 1000.0) # going to wait a bit between events
-            self.client.get("/basket.html")
-            order_post = self.client.post("/orders")
-            print order_post
-            print order_post.text
-
-    @task(3) # this'll make a new user (just to keep things interesting)
-    def populate_data(self):
+    @task(prob_distr['register'] * 100)
+    def register(self):
         #print "about to populate this database!"
         # first register
         username = gen_random()
@@ -105,26 +115,6 @@ class BackgroundTraffic(TaskSet):
         addressObject = {"street": "Whitelees Road", "number": "246", "country": "United Kingdom", "city": "Glasgow",
                          "postcode": "G67 3DL", "id": userID}
         cAddr = self.client.post("/addresses", json=addressObject)
-    #'''
-    ## COPY-PASTED FROM HERE: https://github.com/microservices-demo/load-test/blob/master/locustfile.py
-    ## I DID NOT WRITE THIS ONE--- it's from the offical repo
-    @task(1)
-    def load(self):
-        base64string = base64.encodestring('%s:%s' % ('user', 'password')).replace('\n', '')
-
-        catalogue = self.client.get("/catalogue").json()
-        category_item = choice(catalogue)
-        item_id = category_item["id"]
-
-        self.client.get("/")
-        self.client.get("/login", headers={"Authorization":"Basic %s" % base64string})
-        self.client.get("/category.html")
-        self.client.get("/detail.html?id={}".format(item_id))
-        self.client.delete("/cart")
-        self.client.post("/cart", json={"id": item_id, "quantity": 1})
-        self.client.get("/basket.html")
-        self.client.post("/orders")
-
 
 class GenBackgroundTraffic(HttpLocust):
     print "Can I see this??" # yes, yes I can
