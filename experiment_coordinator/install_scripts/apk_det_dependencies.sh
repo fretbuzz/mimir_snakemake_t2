c1702c3dda22d1a0b91c5da1de70927c7777e172
fretbuzz <jseverin@andrew.cmu.edu>
2019-09-06 18:20:21 -0400

    does this fix it??

--- experiment_coordinator/install_scripts/apk_det_dependencies.sh
+++ experiment_coordinator/install_scripts/apk_det_dependencies.sh
@@ -12,6 +12,7 @@ export GIT_SSL_NO_VERIFY=1
 git clone https://github.com/fretbuzz/DET /DET
 git -C /DET pull
 pip2 install 'certifi==2015.4.28' --force-reinstall
+pip install requests
 pip install -r /DET/requirements.txt --user
 
 pip install pycrypto
--- experiment_coordinator/install_scripts/apt_det_dependencies.sh
+++ experiment_coordinator/install_scripts/apt_det_dependencies.sh
@@ -9,6 +9,7 @@ git clone https://github.com/fretbuzz/DET /DET
 git -C /DET pull
 pip2 install 'certifi==2015.4.28' --force-reinstall
 pip install -r /DET/requirements.txt --user
+pip install requests
 
 pip install pycrpyto
 if [ $? -eq 0 ]; then
