05c8318892ffaf76a9935c6343565ef23cecdc5d
fretbuzz <jseverin@andrew.cmu.edu>
2019-09-13 15:50:01 -0400

    was missing alot of necessary steps

--- configs_to_reproduce_results/e2e_repro_scripts/sockshop_repo_PHYSICAL.sh
+++ configs_to_reproduce_results/e2e_repro_scripts/sockshop_repo_PHYSICAL.sh
@@ -7,9 +7,18 @@ bash ../configs_to_reproduce_results/kubernetes_setup_script.sh
 # need to install DET locally as part of physical exfil component
 sudo git clone https://github.com/fretbuzz/DET.git /DET/
 
-sudo python run_experiment.py --prepare_app --config_file ../configs_to_reproduce_results/Data_Collection/Sockshop/Scale_Physical/sockshop_four_100_exp.json ;\
+sudo python run_experiment.py --config_file ../configs_to_reproduce_results/Data_Collection/Sockshop/Scale_Physical/sockshop_four_100_exp.json ;\
 
 sudo python run_experiment.py --config_file ../configs_to_reproduce_results/Data_Collection/Sockshop/Scale_Physical/sockshop_four_100_mk2_exp.json ;\
+
+sleep 60
+
+cd ../analysis_pipeline/
+
+## then install the analysis pipeline depdencies
+. ../configs_to_reproduce_results/install_mimir_depend_scripts.sh
+
+sudo python generate_paper_graphs.py --config_json ../configs_to_reproduce_results/Data_Analysis/Sockshop/Scale_PHYSICAL/sockshop_scale_physical.json ;\
 
 exit 1
 
