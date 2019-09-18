d9f05ebd763bcd4e30f8ddade9ed61b3dc84b710
fretbuzz <jseverin@andrew.cmu.edu>
2019-09-18 17:15:38 -0400

    getting there...

--- analysis_pipeline/dlp_pipeline.py
+++ analysis_pipeline/dlp_pipeline.py
@@ -405,7 +405,7 @@ def end_to_end_microservice(train_experimental_config, test_experimental_config,
         testing_performance = calculate_performance_metrics(testing_alert_timestamps, test_exfil_periods, alert_granularity,
                                                          start_time, end_time)
         testing_performance_vals[alert_granularity] = pd.DataFrame(testing_performance_vals, index=[0])
-        testing_performance_vals[alert_granularity]['exfil_weights'] = []
+        testing_performance_vals[alert_granularity]['exfil_weights'] = [[]]
 
 
     return testing_performance_vals
