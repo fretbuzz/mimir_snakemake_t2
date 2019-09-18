3b3344e7ee8684e4bd7bcde35184bc81e4e92b1c
fretbuzz <jseverin@andrew.cmu.edu>
2019-09-18 15:15:29 -0400

    plz work

--- analysis_pipeline/dlp_pipeline.py
+++ analysis_pipeline/dlp_pipeline.py
@@ -453,6 +453,8 @@ def calculate_performance_metrics(alert_timestamps, exfil_periods, alert_granula
 
 def is_in_exfil_period(exfil_periods, bin_edge, alert_granularity):
     for exfil_period in exfil_periods:
+        if exfil_period == []:
+            continue
         print "cur_exfil_period", exfil_period
         exfil_start, exfil_end = exfil_period
         ''' # eh, this code block may or may not be correct, but it's certainly bad either way
