ac8b313a14e8bfb0c564d0b2def92261b194d8d6
fretbuzz <jseverin@andrew.cmu.edu>
2019-09-19 11:52:27 -0400

    new attempt...

---
+++ analysis_pipeline/debugging_attempt.py
@@ -0,0 +1,11 @@
+import pickle, sys
+import pandas as pd
+
+if __name__=="__main__":
+    print "RUNNING"
+    print sys.argv
+
+    with open('./check_this.txt', 'r') as f:
+        cont = f.read()
+
+    pd_df = pickle.loads(cont)
