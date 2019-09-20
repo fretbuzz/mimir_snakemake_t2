557c59a6dca2f1651a7b640bc8a1396ebcf525b6
fretbuzz <jseverin@andrew.cmu.edu>
2019-09-20 10:47:05 -0400

    new version of minikube is breaking things, so let's just use the old version'

--- configs_to_reproduce_results/kubernetes_setup_script.sh
+++ configs_to_reproduce_results/kubernetes_setup_script.sh
@@ -22,7 +22,7 @@ sudo apt-get install virtualbox-6.0 -y
 
 # okay, onto the next step
 # First, we'll install and start Minikube
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64   && chmod +x minikube
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v1.3.1/minikube-linux-amd64  && chmod +x minikube
 sudo cp minikube /usr/local/bin && rm minikube
 
 minikube start --vm-driver=virtualbox --cpus=12 --memory=32000 --disk-size 65g
