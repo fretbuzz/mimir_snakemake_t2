c8ff7f65d3d0b1d669da2913005634785ddc1e99
fretbuzz <jseverin@andrew.cmu.edu>
2019-11-19 10:40:52 -0500

    improved hipsterStore e2e repro script

--- configs_to_reproduce_results/e2e_repro_scripts/hipsterStore_repro.sh
+++ configs_to_reproduce_results/e2e_repro_scripts/hipsterStore_repro.sh
@@ -2,33 +2,54 @@
 ### HipsterStore REPRODUCABILITY -- SCALE ###
 # NOTE: adservice doesn't work-- but need to wait for the dev's to fix it...
 
-# this should be applicable to everyone...
-bash ../configs_to_reproduce_results/kubernetes_setup_script.sh | tee kubernetes_setup.log;
+if [ $# -gt 2 ]; then
+  echo "too many args";
+  exit 2;
+fi
 
-sudo python -u run_experiment.py --no_exfil --prepare_app --config_file ../configs_to_reproduce_results/Data_Collection/HipsterStore/Scale/hipsterStore_100_mk2_exp.json | tee hipsterStore_100_mk2.log;
-sudo python -u run_experiment.py --no_exfil --config_file ../configs_to_reproduce_results/Data_Collection/HipsterStore/Scale/hipsterStore_100_exp.json | tee hipsterStore_100.log;
-sudo python -u run_experiment.py --no_exfil --config_file ../configs_to_reproduce_results/Data_Collection/HipsterStore/Scale/hipsterStore_120_exp.json | tee hipsterStore_120.log;
-sudo python -u run_experiment.py --no_exfil --config_file ../configs_to_reproduce_results/Data_Collection/HipsterStore/Scale/hipsterStore_140_exp.json | tee hipsterStore_140.log;
+skip_pcap=0
+if [ "$1" == "--skip_pcap" ]; then
+  skip_pcap=1
+fi
 
-sleep 60
+echo "skip_pcap $skip_pcap"
 
-bash ../configs_to_reproduce_results/cycle_minikube.sh
+if [ $skip_pcap -eq 0 ]
+then
 
-sleep 120
+  # this should be applicable to everyone...
+  bash ../configs_to_reproduce_results/kubernetes_setup_script.sh | tee kubernetes_setup.log;
 
-sudo python -u run_experiment.py --no_exfil --prepare_app --config_file ../configs_to_reproduce_results/Data_Collection/HipsterStore/Scale/hipsterStore_160_exp.json | tee hipsterStore_160.log;
-sudo python -u run_experiment.py --no_exfil --config_file ../configs_to_reproduce_results/Data_Collection/HipsterStore/Scale/hipsterStore_80_exp.json | tee hipsterStore_80.log;
-sudo python -u run_experiment.py --no_exfil --config_file ../configs_to_reproduce_results/Data_Collection/HipsterStore/Scale/hipsterStore_60_exp.json | tee hipsterStore_60.log;
-sudo python -u run_experiment.py --no_exfil --config_file ../configs_to_reproduce_results/Data_Collection/HipsterStore/Scale/hipsterStore_40_exp.json | tee hipsterStore_40.log;
+  sudo python -u run_experiment.py --no_exfil --prepare_app --config_file ../configs_to_reproduce_results/Data_Collection/HipsterStore/Scale/hipsterStore_100_mk2_exp.json | tee hipsterStore_100_mk2.log;
+  sudo python -u run_experiment.py --no_exfil --config_file ../configs_to_reproduce_results/Data_Collection/HipsterStore/Scale/hipsterStore_100_exp.json | tee hipsterStore_100.log;
+  sudo python -u run_experiment.py --no_exfil --config_file ../configs_to_reproduce_results/Data_Collection/HipsterStore/Scale/hipsterStore_120_exp.json | tee hipsterStore_120.log;
+  sudo python -u run_experiment.py --no_exfil --config_file ../configs_to_reproduce_results/Data_Collection/HipsterStore/Scale/hipsterStore_140_exp.json | tee hipsterStore_140.log;
 
-sleep 60
+  sleep 60
+
+  bash ../configs_to_reproduce_results/cycle_minikube.sh
+
+  sleep 120
+
+  sudo python -u run_experiment.py --no_exfil --prepare_app --config_file ../configs_to_reproduce_results/Data_Collection/HipsterStore/Scale/hipsterStore_160_exp.json | tee hipsterStore_160.log;
+  sudo python -u run_experiment.py --no_exfil --config_file ../configs_to_reproduce_results/Data_Collection/HipsterStore/Scale/hipsterStore_80_exp.json | tee hipsterStore_80.log;
+  sudo python -u run_experiment.py --no_exfil --config_file ../configs_to_reproduce_results/Data_Collection/HipsterStore/Scale/hipsterStore_60_exp.json | tee hipsterStore_60.log;
+  sudo python -u run_experiment.py --no_exfil --config_file ../configs_to_reproduce_results/Data_Collection/HipsterStore/Scale/hipsterStore_40_exp.json | tee hipsterStore_40.log;
+
+  sleep 60
+
+  sudo minikube stop || true
+
+fi
 
 cd ../analysis_pipeline/
 
 ## then install the analysis pipeline depdencies
 . ../configs_to_reproduce_results/install_mimir_depend_scripts.sh
 
-sudo python -u generate_paper_graphs.py --config_json ../configs_to_reproduce_results/Data_Analysis/HipsterStore/Scale/hipsterStore_scale.json | tee hipsterStore_repro.log;
+mkdir multilooper_outs
+
+sudo python -u generate_paper_graphs.py --config_json ../configs_to_reproduce_results/Data_Analysis/HipsterStore/Scale/hipsterStore_scale.json | tee tee ./multilooper_outs/hipsterStore_repro.log;
 
 
 echo "okieee, all done :)"
