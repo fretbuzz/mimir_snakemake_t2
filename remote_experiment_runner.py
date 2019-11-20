410c2e27c746f05abb99eef45e0137499f0fb23b
fretbuzz <jseverin@andrew.cmu.edu>
2019-11-20 11:31:25 -0500

    config mistake in sockshop setup

--- remote_experiment_runner.py
+++ remote_experiment_runner.py
@@ -344,9 +344,9 @@ if __name__=="__main__":
 
         #config_file_pth = "./remote_experiment_configs/sockshop_scale_newRepro.json"
 
-        config_file_pth = "./remote_experiment_configs/wordpress_scale_trial_1.json"
+        #config_file_pth = "./remote_experiment_configs/wordpress_scale_trial_1.json"
         #config_file_pth = "./remote_experiment_configs/wordpress_scale_trial_2.json"
-        #config_file_pth = "./remote_experiment_configs/wordpress_scale_trial_3.json"
+        config_file_pth = "./remote_experiment_configs/wordpress_scale_trial_3.json"
         #config_file_pth = './remote_experiment_configs/sockshop_scale_test1.json'
 
     else:
