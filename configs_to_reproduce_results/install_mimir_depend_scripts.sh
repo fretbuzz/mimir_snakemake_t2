eb0c0c442246c92ff471eae787dd79c71d9310af
fretbuzz <jseverin@andrew.cmu.edu>
2020-02-24 16:18:55 -0500

    I think that this might be causing problems...

--- configs_to_reproduce_results/install_mimir_depend_scripts.sh
+++ configs_to_reproduce_results/install_mimir_depend_scripts.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #curl -fsSL https://get.docker.com -o get-docker.sh
 #sudo sh get-docker.sh
+sudo apt-get update
 sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
