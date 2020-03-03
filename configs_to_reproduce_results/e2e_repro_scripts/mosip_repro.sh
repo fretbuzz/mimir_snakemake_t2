a241341570ff7782bc9dc5cdcd464da79eeb40c7
fretbuzz <jseverin@andrew.cmu.edu>
2020-03-03 12:16:29 -0500

    working on it...

--- configs_to_reproduce_results/e2e_repro_scripts/mosip_repro.sh
+++ configs_to_reproduce_results/e2e_repro_scripts/mosip_repro.sh
@@ -18,52 +18,16 @@ echo "skip_pcap $skip_pcap"
 if [ $skip_pcap -eq 0 ]
 then
 
-  # this should be applicable to everyone...
-  # vv I think not needed... MOSIP launcer will do it all for us...
-  #bash ../configs_to_reproduce_results/kubernetes_setup_script_centos.sh | tee kubernetes_setup.log;
-  sudo pip uninstall pyOpenSSL
-  # TODO: honestly, using MOSIP launcher is probably easier...
-  ### yes, we'll need an LDAP server... try following this tutorial: https://linuxhostsupport.com/blog/how-to-install-ldap-on-centos-7/
-  '''
-  sudo yum install https://releases.ansible.com/ansible/rpm/release/epel-7-x86_64/ansible-2.7.9-1.el7.ans.noarch.rpm
-  sudo easy_install pip
-  sudo pip install jmespath==0.9.4
-  '''
-  git clone https://github.com/mosip/mosip-platform.git
-  git clone https://github.com/mosip/mosip-ref-impl.git
-  git clone https://github.com/mosip/mosip-config.git
-  git clone https://github.com/mosip/mosip-infra.git
-  ################# probably do NOT do this... probably too complicated... just use MOSIP launcher...
-  '''
-  sudo yum install -y maven
-  cd ./mosip-platform
-  #mvn clean install
-  # TODO: should automate this (i did it manually...)
-    # (i) remove authentication/authentication-demo-app
-    # (2) remove bioSDK part from registration/registration-services/pom.xml
-  mvn clean install -Dmaven.test.skip=true
-
-  # TODO: add code to setup private docker registry and build MOSIP components...
-  # deploy registry docker
-  run -d -p 5000:5000 --restart always --name registry registry:2
-  # then push from the maven artificat repository to the docker registry
-
-  # TODO push the moven images to the docker registry
-  # docker image list (hopefully it shows up... if it does then we push to the registry...)
-  # ^^ just to check during development, not part of long-term workflow...
-
-  # then modify the kubernetes config files to grab from our docker registery
-  # TODO
-
-  # finaly, execute kubectl apply to those files...
-  # TODO
-  '''
-  ###################
-
-  # https://get.helm.sh/helm-v3.0.0-linux-amd64.tar.gz
-  # tar -zxvf helm-v3.0.0-linux-amd64.tar.gz
-  # /usr/local/bin/helm
-
+  # use this method for deployment https://github.com/mosip/mosip-infra/tree/master/deployment/sandbox
+  # note: this is all untested and will certainly fail...
+  git clone https://github.com/mosip/mosip-infra
+  sudo su
+  # TODO: replace the vars...
+  cd mosip-infra/deployment/sandbox/
+  sudo sh install-mosip-kernel.sh
+  sleep 360
+  sudo sh install-mosip-pre-reg.sh
+  seep 720
 
   # TODO: run the MOSIP experiments here...
 
